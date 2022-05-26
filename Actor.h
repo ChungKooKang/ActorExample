5f15297ce5df7b155c481f92993370f553493d6d
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-05-26 16:54:36 +0900

    actor 기본 setting입니다.

--- Actor.h
+++ Actor.h
@@ -1,5 +1,31 @@
 #pragma once
+
+#include "D2DFramework.h"
 class Actor
 {
+protected :
+	D2DFramework* mpFramework;
+	Microsoft::WRL::ComPtr<ID2D1Bitmap> mspBitmap;
+
+	float mX;
+	float mY;
+	float mOpacity;
+
+public :
+
+	// 생성자
+	Actor() = delete;
+	Actor(D2DFramework* pFramework, LPCWSTR filename);
+	Actor(D2DFramework* pFramework, LPCWSTR filename, float x, float y, float opacity = 1.0f);
+
+	// 소멸자
+	virtual ~Actor();
+
+	virtual void Draw();
+
+private :
+
+	HRESULT LoadWICImage(LPCWSTR filename);
+	void Draw(float x, float y, float opacity = 1.0f); // 외부에 쓰는 Draw를 간소화하게 이렇게 만들어서 private의 함수를 wrapping 할 수 있음.
 };
 
